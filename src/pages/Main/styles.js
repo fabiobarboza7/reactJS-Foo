9f2128196758d207e66583b0109213c7456d1bd3
Fábio Barboza Coelho de Souza <fabiobarboza7@hotmail.com>
2019-08-01 17:17:04 -0300

    setup styled components

---
+++ src/pages/Main/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  font-size: 24px;
+  color: ${props => (props.fabio ? 'red' : 'green')}
+  font-family: Arial, Helvetica, sans-serif;
+`;
